20612d4b3a8089620b133914485fbea09cc4d694
initials101 <muriithidennis340@gmail.com>
2025-04-11 15:49:55 +0300

    commit update

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -185,14 +185,14 @@ const images = [
       </h2>
       <div className="flex-grow"> {}
         <p className="text-gray-600 mb-4 text-[1.1rem] font-montserrat">
-          Health Nest is a cutting-edge healthcare management system
+          MEDREF is a cutting-edge healthcare management system
           designed to streamline medical processes and enhance patient
           care. Our platform integrates advanced technology with medical
           expertise to provide a seamless experience for both healthcare
           providers and patients.
         </p>
         <p className="text-gray-600 mb-4 text-[1.1rem] font-montserrat">
-          With Health Nest, you can easily manage appointments, access
+          With MEDREF, you can easily manage appointments, access
           medical records, and communicate with your healthcare team.
           We're committed to improving healthcare accessibility and
           efficiency, ensuring that you receive the best possible care.
